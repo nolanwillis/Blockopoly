3dc44552e5ee39d25c275edf551932c18e669b25
Nolan Willis <nolanwillis38@gmail.com>
2023-03-18 12:00:41 -0400

    initial

---
+++ Source/Blockopoly/BlockopolyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlockopolyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOCKOPOLY_API ABlockopolyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
